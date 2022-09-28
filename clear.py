2157802e01a3e7050ae9705a81858579d2414a25
Jonah Bebko <jonahbebko@outlook.com>
2022-09-28 06:50:12 -0500

    window with label and event detector

--- clear.py
+++
@@ -1,8 +0,0 @@
-from os import system
-from os import name
-
-# fancy operating system detection to clear the terminal
-# but this is a windows-only application
-# kekw
-def clear():
-    system("cls" if name in ["nt", "dos"] else "clear")
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import search
 import download
-import tk
+from tkinter import *
+from tkinter import ttk
+
+def buttonPressed(*args):
+
+    print("button pressed")
 
 def main():
-    print("kys")
+
+    root = Tk()
+    root.title("Python Audio YouTube Downloader")
+
+    l = ttk.Label(root, text="Starting")
+    l.grid()
+
+    l.bind('<Enter>', lambda e: l.configure(text='Moved mouse inside'))
+    l.bind('<Leave>', lambda e: l.configure(text='Moved mouse outside'))
+    l.bind('<ButtonPress-1>', buttonPressed)
+
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
