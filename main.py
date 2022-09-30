dd60d84afa6eea8d4d9cb6ef3bf645b63f32843c
Replit user <>
2022-09-30 14:26:16 +0000

    changed back to pack

--- main.py
+++ main.py
@@ -23,32 +23,32 @@ def main(*args, **kwargs):
 
     root = Tk()
     root.title("Python Audio YouTube Downloader")
-    root.geometry("360x240")
+    root.geometry("600x240")
     root.minsize(width=200, height=600)
     root.iconphoto(False, PhotoImage(file = "icon.ico"))
 
     frame = Frame(root)
-    frame.pack(fill="x")
+    frame.pack()
 
     s = Style().configure("WBStyle",background="black",foreground="white")
 
     searchEntryLabel = Label(frame, text="Search entry or URL:")
-    searchEntryLabel.grid(padx=10, pady=(10,0), row=1, column=1)
+    searchEntryLabel.pack(fill='x', side=LEFT)
 
     searchEntryText = StringVar()
     searchEntry = Entry(frame, justify=LEFT, textvariable=searchEntryText)
-    searchEntry.grid(padx=10, row=2, column=1)
+    searchEntry.pack(fill='x', side=LEFT)
     searchEntry.focus()
 
     numResultsLabel = Label(frame, text="Number of results:")
-    numResultsLabel.grid(padx=10, pady=(10,0), row=3, column=1)
+    numResultsLabel.pack(fill='x', side=LEFT)
 
     numResults = StringVar()
     resultsEntry = Entry(frame, justify=LEFT, textvariable=numResults)
-    resultsEntry.grid(padx=10, row=4, column=1)
+    resultsEntry.pack(fill='x', side=LEFT)
 
     searchButton = Button(text="Search")
-    searchButton.grid(padx=10, row=4, column=2)
+    searchButton.pack(fill='x', side=RIGHT)
     searchButton.bind('<ButtonPress-1>', searchButtonPressed)
 
     # custom Windows 11-like theme, pretty cool
