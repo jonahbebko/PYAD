28fbc46caf4de76cfbd9e0e8c9f4824fcf90d8d4
Jonah Bebko <jonahbebko@outlook.com>
2022-10-03 02:20:35 -0400

    finished, after ten hours

--- lib/download.py
+++ lib/download.py
@@ -2,11 +2,5 @@ from pytube import YouTube as yt
 import os
 
 def download(id, path="."):
-    vid = yt("https://youtu.be/" + id).streams.filter(only_audio=True).first()
-    save = vid.download(output_path=path)
-
-    # save memory by using a void variable
-    # but this is python so who really cares about memory
-    # it's about looking cool and smart
-    base, _ = os.path.splitext(save)
-    os.rename(save, base + ".mp3")
+    os.rename(yt("https://youtu.be/" + id).streams.filter(only_audio=True).first().download(output_path=path), "output.mp3")
+    # one-line expressions are funny
--- lib/search.py
+++ lib/search.py
@@ -1,13 +1,21 @@
 from youtubesearchpython import VideosSearch as vs
 
-def search(title:str, num:int=5):
+def search(title:str, num:int):
+
+    if not num:
+        num = 5
+
+    if "youtube.com" in title or "youtu.be" in title:
+        num = 1
 
     # initialize lists (yes it hurts to look at, i know)
-    durs, views, channels, titles, ids = [], [], [], [], []
+    thumbs, cthumbs, durs, views, channels, titles, fulltitles, ids = [], [], [], [], [], [], [], []
 
     # search for video and add data to lists
-    for video in vs(title, limit=num).result()['result']:
+    for video in vs(title, limit=int(num)).result()['result']:
 
+        thumbs.append(video['thumbnails'][0]['url'])
+        cthumbs.append(video['channel']['thumbnails'][0]['url'])
         durs.append(video['duration'])
         views.append(video['viewCount']['short'])
 
@@ -16,6 +24,8 @@ def search(title:str, num:int=5):
             channels.append(video['channel']['name'][:22] + "...")
         else:
             channels.append(video['channel']['name'])
+        
+        fulltitles.append(video['title'])
 
         if len(video['title']) > 40:
             titles.append(video['title'][:37] + '...')
@@ -24,5 +34,62 @@ def search(title:str, num:int=5):
         
         ids.append(video['id'])
     
-    # return the id list since it's used for the downloader
-    return ids
+    return thumbs, cthumbs, durs, views, channels, titles, fulltitles, ids
+
+
+
+
+# VIDEO RESULT FORMAT
+
+{
+    'type': 'video',
+    'id': '2ZIpFytCSVc',
+    'title': 'Bruh Sound Effect #2',
+    'publishedTime': '6 years ago',
+    'duration': '0:02',
+    'viewCount':
+    {
+        'text': '27,914,126 views', 'short': '27M views'
+    },
+    'thumbnails':
+    [
+        {
+            'url': 'https://i.ytimg.com/vi/2ZIpFytCSVc/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAYHTc2j9kqEi8idTXfcITHk2Xb9g',
+            'width': 360,
+            'height': 202
+        },
+        {
+            'url': 'https://i.ytimg.com/vi/2ZIpFytCSVc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOMBKHf7tZoVJ4bIizNlJDvE1_SA',
+            'width': 720,
+            'height': 404
+        }
+    ],
+    'richThumbnail': None,
+    'descriptionSnippet':
+    [
+        {
+            'text': 'Leave A Like And Subscribe To Keep Up On The Latest New Free Sound Effects.'
+        }
+    ],
+    'channel':
+    {
+        'name': 'Jame Benedict',
+        'id': 'UCU8N6si9tf2OBSonH_B9Hxg',
+        'thumbnails':
+        [
+            {
+                'url': 'https://yt3.ggpht.com/ytc/AMLnZu8RG1lBREM_wDw0lXC7hQpJF_p0dZ2dlBqBsACw=s68-c-k-c0x00ffffff-no-rj',
+                'width': 68,
+                'height': 68
+            }
+        ],
+        'link': 'https://www.youtube.com/channel/UCU8N6si9tf2OBSonH_B9Hxg'
+    },
+    'accessibility':
+    {
+        'title': 'Bruh Sound Effect #2 by Jame Benedict 6 years ago 2 seconds 27,914,126 views',
+        'duration': '2 seconds'
+    },
+    'link': 'https://www.youtube.com/watch?v=2ZIpFytCSVc',
+    'shelfTitle': None
+}
