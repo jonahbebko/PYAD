80a4c3bf8a916645fc89169355e329d956590355
Jonah Bebko <jonahbebko@outlook.com>
2022-10-05 21:04:52 -0400

    updated paths

--- build.bat
+++ build.bat
@@ -1,2 +1,1 @@
-@echo off
-"%LocalAppData%\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts\pyinstaller.exe" "main.spec"
+"%LocalAppData%\Programs\Python\Python310\Scripts\pyinstaller.exe" "main.spec"
